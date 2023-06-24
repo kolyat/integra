ae990c490de4c74b15adb5f857d986d60f509068
Kolyat <kks.pub@gmail.com>
2023-06-25 00:15:50 +0300

    Add READMEs to build

--- build.bat
+++ build.bat
@@ -1,8 +1,19 @@
+set author=Kolyat
+
 pyinstaller --clean ./utils/setpwd.spec
 pyinstaller --clean integra.spec
 
 mkdir ".\dist\utils"
 mkdir ".\dist\utils\bundletool"
+
+mdpdf --author %author% ^
+  --title "Integra general README" ^
+  --output ".\dist\README.pdf" ^
+  "README.md"
+mdpdf --author %author% ^
+  --title "Integra bundletool README" ^
+  --output ".\dist\utils\bundletool\README.pdf" ^
+  ".\utils\bundletool\README.md"
 
 move /y ".\dist\setpwd.exe" ".\dist\utils"
 copy /y ".\utils\bundletool\*.*" ".\dist\utils\bundletool"
